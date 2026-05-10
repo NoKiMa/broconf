ab8f0fbee1ebe9aefdf0dadffa6b07f7647d6543
NoKi <novak.kirill@gmail.com>
2026-05-10 16:56:05 +0300

    chore: update EVENT_DATE to 17:00:00

--- constants/event.ts
+++ constants/event.ts
@@ -1,5 +1,6 @@
 // The target date for the countdown.
 // Format: YYYY-MM-DDTHH:MM:SS (in local time or specify timezone)
-export const EVENT_DATE = '2026-05-11T15:00:00';
+export const EVENT_DATE = '2026-05-11T17:00:00';
 export const EVENT_DATE_END = '2026-05-13T15:00:00';  
 export const EVENT_DATE_NOW = '2027-05-11T15:00:00';
+ 
